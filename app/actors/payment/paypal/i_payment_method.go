1a0955e7d31b56355943a4f73382c4e59f3d20f2
Viktor Nazarov <vnazarov@speroteck.com>
2014-08-20 04:38:09 -0700

    ENG-165: Paypal GetAccessToken function

--- app/actors/payment/paypal/i_payment_method.go
+++ app/actors/payment/paypal/i_payment_method.go
@@ -1,7 +1,16 @@
 package paypal
 
 import (
+	"errors"
+	"bytes"
+
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
+
 	"github.com/ottemo/foundation/app/models/checkout"
+	"github.com/ottemo/foundation/app/utils"
+
 )
 
 func (it *PayPal) GetName() string {
@@ -35,4 +44,42 @@ func (it *PayPal) Refund() error {
 
 func (it *PayPal) Void()	error {
 	return nil
+}
+
+
+// returns application access token needed for all other requests
+func GetAccessToken() (string, error) {
+	body := "grant_type=client_credentials"
+	req, err := http.NewRequest("POST", "https://api.sandbox.paypal.com/v1/oauth2/token", bytes.NewBufferString(body))
+	if err != nil {
+		return "", err
+	}
+
+	req.SetBasicAuth("AbrcnhDi238ke9aG2NIQqVkW90oMJVg3B1QsjC68d2xRBLDq8boIrCaigPli", "EPcLWBCmfM_AwSOO1jC6TEDLCg-xZhFrUmXQnvTQ9yZV5_786xc5OkQ4Gx2-")
+
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Accept-Language", "en_US")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	buf, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	result := make(map[string]interface{})
+	err = json.Unmarshal(buf, &result)
+	if err != nil {
+		return "", err
+	}
+
+	if token, present := result["access_token"]; present {
+		return  utils.InterfaceToString(token), nil
+	}
+
+	return "", errors.New("unexpected response - without access_token")
 }
