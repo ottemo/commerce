2bdb9edee94e0c84ce3d355fd83026dae2c7f2bc
Viktor Nazarov <vnazarov@speroteck.com>
2014-09-16 07:48:35 -0700

    ENG-203: more configuration values for site

--- app/config.go
+++ app/config.go
@@ -3,6 +3,7 @@ package app
 import (
 	"errors"
 	"github.com/ottemo/foundation/env"
+	"github.com/ottemo/foundation/app/models/checkout"
 )
 
 // setup configuration values
@@ -19,7 +20,7 @@ func setupConfig() error {
 		Editor:      "",
 		Options:     nil,
 		Label:       "General",
-		Description: "Application general options",
+		Description: "application general options",
 		Image:       "",
 	}, nil)
 
@@ -34,7 +35,7 @@ func setupConfig() error {
 		Editor:      "",
 		Options:     nil,
 		Label:       "Application",
-		Description: "Application specific options",
+		Description: "application related options",
 		Image:       "",
 	}, nil)
 
@@ -80,6 +81,187 @@ func setupConfig() error {
 		Options:     nil,
 		Label:       "Foundation host URL",
 		Description: "URL application will use to generate foundation resources links",
+		Image:       "",
+	}, nil)
+
+	if err != nil {
+		return err
+	}
+
+	err = config.RegisterItem(env.T_ConfigItem{
+		Path:        CONFIG_PATH_STORE_GROUP,
+		Value:       nil,
+		Type:        env.CONFIG_ITEM_GROUP_TYPE,
+		Editor:      "",
+		Options:     nil,
+		Label:       "Store",
+		Description: "web store related options",
+		Image:       "",
+	}, nil)
+
+	if err != nil {
+		return err
+	}
+
+	err = config.RegisterItem(env.T_ConfigItem{
+		Path:        CONFIG_PATH_STORE_NAME,
+		Value:       "Ottemo store",
+		Type:        "varchar(255)",
+		Editor:      "text",
+		Options:     nil,
+		Label:       "Name",
+		Description: "name of your web store",
+		Image:       "",
+	}, nil)
+
+	if err != nil {
+		return err
+	}
+
+	err = config.RegisterItem(env.T_ConfigItem{
+		Path:        CONFIG_PATH_STORE_EMAIL,
+		Value:       "store@ottemo.io",
+		Type:        "varchar(255)",
+		Editor:      "text",
+		Options:     nil,
+		Label:       "E-mail",
+		Description: "e-mail of your web store",
+		Image:       "",
+	}, nil)
+
+	if err != nil {
+		return err
+	}
+
+	err = config.RegisterItem(env.T_ConfigItem{
+		Path:        CONFIG_PATH_STORE_COUNTRY,
+		Value:       "US",
+		Type:        "string",
+		Editor:      "select",
+		Options:     checkout.COUNTRIES_LIST,
+		Label:       "Country",
+		Description: "store location country",
+		Image:       "",
+	}, nil)
+
+	if err != nil {
+		return err
+	}
+
+	err = config.RegisterItem(env.T_ConfigItem{
+		Path:        CONFIG_PATH_STORE_STATE,
+		Value:       "",
+		Type:        "string",
+		Editor:      "select",
+		Options:     checkout.STATES_LIST,
+		Label:       "State",
+		Description: "store location state",
+		Image:       "",
+	}, nil)
+
+	if err != nil {
+		return err
+	}
+
+	err = config.RegisterItem(env.T_ConfigItem{
+		Path:        CONFIG_PATH_STORE_CITY,
+		Value:       "",
+		Type:        "string",
+		Editor:      "line_text",
+		Options:     "",
+		Label:       "City",
+		Description: "store location city",
+		Image:       "",
+	}, nil)
+
+	if err != nil {
+		return err
+	}
+
+	err = config.RegisterItem(env.T_ConfigItem{
+		Path:        CONFIG_PATH_STORE_ADDRESSLINE1,
+		Value:       "",
+		Type:        "string",
+		Editor:      "line_text",
+		Options:     "",
+		Label:       "Address Line 1",
+		Description: "store location address line 1",
+		Image:       "",
+	}, nil)
+
+	if err != nil {
+		return err
+	}
+
+	err = config.RegisterItem(env.T_ConfigItem{
+		Path:        CONFIG_PATH_STORE_ADDRESSLINE2,
+		Value:       "",
+		Type:        "string",
+		Editor:      "line_text",
+		Options:     "",
+		Label:       "Address Line 2",
+		Description: "store location address line 2",
+		Image:       "",
+	}, nil)
+
+	if err != nil {
+		return err
+	}
+
+	err = config.RegisterItem(env.T_ConfigItem{
+		Path:        CONFIG_PATH_STORE_ZIP,
+		Value:       "",
+		Type:        "string",
+		Editor:      "line_text",
+		Options:     "",
+		Label:       "zip",
+		Description: "store location zip code",
+		Image:       "",
+	}, nil)
+
+	if err != nil {
+		return err
+	}
+
+
+	err = config.RegisterItem(env.T_ConfigItem{
+		Path:        CONFIG_PATH_MAIL_GROUP,
+		Value:       nil,
+		Type:        env.CONFIG_ITEM_GROUP_TYPE,
+		Editor:      "",
+		Options:     nil,
+		Label:       "Mail",
+		Description: "web store mailing options",
+		Image:       "",
+	}, nil)
+
+	if err != nil {
+		return err
+	}
+
+	err = config.RegisterItem(env.T_ConfigItem{
+		Path:        CONFIG_PATH_MAIL_SERVER,
+		Value:       nil,
+		Type:        "varchar(255)",
+		Editor:      "text",
+		Options:     nil,
+		Label:       "Host",
+		Description: "web store mailing server",
+		Image:       "",
+	}, nil)
+
+	if err != nil {
+		return err
+	}
+
+	err = config.RegisterItem(env.T_ConfigItem{
+		Path:        CONFIG_PATH_MAIL_PORT,
+		Value:       nil,
+		Type:        env.CONFIG_ITEM_GROUP_TYPE,
+		Editor:      "",
+		Options:     nil,
+		Label:       "Port",
+		Description: "web store mailing server port",
 		Image:       "",
 	}, nil)
 
