47ee171f16f0eb1f5981550a807787bf2a0f33fe
OBesarab <obesarab@speroteck.com>
2015-05-18 09:24:24 -0500

    [#92722144] simple function on success

--- app/actors/checkout/internal.go
+++ app/actors/checkout/internal.go
@@ -1,13 +1,18 @@
 package checkout
 
 import (
+	"fmt"
 	"github.com/ottemo/foundation/api"
 	"github.com/ottemo/foundation/app"
 	"github.com/ottemo/foundation/app/models/cart"
 	"github.com/ottemo/foundation/app/models/checkout"
 	"github.com/ottemo/foundation/app/models/order"
 	"github.com/ottemo/foundation/env"
+	"github.com/ottemo/foundation/media"
 	"github.com/ottemo/foundation/utils"
+	"io/ioutil"
+	"net/http"
+	"strings"
 )
 
 // SendOrderConfirmationMail sends an order confirmation email
@@ -88,6 +93,85 @@ func (it *DefaultCheckout) CheckoutSuccess(checkoutOrder order.InterfaceOrder, s
 	//----------------------
 	eventData := map[string]interface{}{"checkout": it, "order": checkoutOrder, "session": session, "cart": currentCart}
 	env.Event("checkout.success", eventData)
+
+	// make asynchronously a request to TrustPilot
+	go func(checkoutOrder order.InterfaceOrder, currentCart cart.InterfaceCart) {
+		cartItems := currentCart.GetItems()
+
+		requestData := make(map[string]interface{})
+
+		requestData["consumer"] = map[string]interface{}{
+			"email": "test" + utils.InterfaceToString(checkoutOrder.Get("customer_email")),
+			"name":  checkoutOrder.Get("customer_name"),
+		}
+		requestData["referenceId"] = checkoutOrder.GetID()
+		requestData["locale"] = "en-US"
+		requestData["products"] = make([]map[string]string, 0)
+
+		// product media removal
+		mediaStorage, err := media.GetMediaStorage()
+		if err != nil {
+			env.LogError(err)
+		}
+
+		for _, productItem := range cartItems {
+			currentProductID := productItem.GetProductID()
+			currentProduct := productItem.GetProduct()
+
+			mediaPath, err := mediaStorage.GetMediaPath("product", currentProductID, "image")
+			if err != nil {
+				env.ErrorDispatch(err)
+			}
+			productOptions := productItem.GetOptions()
+			productBrand := ""
+			if brand, present := productOptions["brand"]; present {
+				productBrand = utils.InterfaceToString(brand)
+			}
+
+			productInfo := map[string]string {
+				"productUrl": app.GetStorefrontURL("product/" + currentProductID),
+				"imageUrl":   app.GetStorefrontURL(mediaPath + currentProduct.GetDefaultImage()),
+				"name":       currentProduct.GetName(),
+				"sku":        currentProduct.GetSku(),
+				"brand":      productBrand,
+			}
+
+			requestData["products"] = append(utils.InterfaceToArray(requestData["products"]), productInfo)
+		}
+		fmt.Println(requestData, currentCart)
+
+		requestURL := "https://api.trustpilot.com/v1/private/product-reviews/business-units/{businessUnitId}/invitation-links"
+		businessUnitID := "test"
+		requestURL = strings.Replace(requestURL, "{businessUnitId}", businessUnitID, 1)
+
+		request, err := http.NewRequest("POST", requestURL, nil)
+		request.Header.Set("Content-Type", "application/json")
+		if err != nil {
+			env.LogError(err)
+		}
+
+		client := &http.Client{}
+		response, err := client.Do(request)
+
+		defer response.Body.Close()
+		if err != nil {
+			env.LogError(err)
+		}
+		responseBody, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			env.LogError(err)
+		}
+
+		jsonResponse, err := utils.DecodeJSONToStringKeyMap(responseBody)
+		if err != nil {
+			env.LogError(err)
+		}
+
+		if result, ok := jsonResponse["result"]; ok {
+			fmt.Println(result)
+		}
+
+	}(checkoutOrder)
 
 	err = it.SendOrderConfirmationMail()
 	if err != nil {
