9a37d33cebbfd9464fe70953c4f8949d735f5ba2
James Vastbinder <james.vastbinder@gmail.com>
2015-06-10 15:32:01 -0700

    clean up imports

--- app/actors/payment/paypal/api.go
+++ app/actors/payment/paypal/api.go
@@ -9,10 +9,11 @@ import (
 
 	"github.com/ottemo/foundation/api"
 	"github.com/ottemo/foundation/app"
-	"github.com/ottemo/foundation/app/models/checkout"
-	"github.com/ottemo/foundation/app/models/order"
 	"github.com/ottemo/foundation/env"
 	"github.com/ottemo/foundation/utils"
+
+	"github.com/ottemo/foundation/app/models/checkout"
+	"github.com/ottemo/foundation/app/models/order"
 )
 
 // setupAPI setups package related API endpoint routines
