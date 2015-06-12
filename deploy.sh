b6c042009aeb4b465403cbd4ad6638812da2125f
maazghani <maazghani+github@gmail.com>
2015-06-12 12:12:30 -0700

    path to built executable and custom branch for testing

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if [ "$BRANCH" == 'develop' ]; then
+if [ "$BRANCH" == 'feature/maaz_deploy_to_host' ]; then
     GIT_COMMIT=`echo $COMMIT | head -c 5` 
-    scp -r $GOROOT/foundation ottemo@$REMOTE_HOST:~/deploy/foundation-$GIT_COMMIT
+    scp -r $GOROOT/bin/foundation ottemo@$REMOTE_HOST:~/deploy/foundation-$GIT_COMMIT
     ssh ottemo@$REMOTE_HOST "cd /home/ottemo/deploy/ && ln -sf foundation-$GIT_COMMIT foundation-latest" 
     ssh ottemo@$REMOTE_HOST "sudo /etc/init.d/ottemo stop && cp ~/foundation/foundation ~/foundation/backup/foundation-$(date +%Y%m%d)"
     ssh ottemo@REMOTE_HOST "cp ~/deploy/foundation-latest ~/foundation/foundation && sudo /etc/init.d/ottemo start"
