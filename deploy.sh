6506958323ca5bfe7f3c0b416b0eb06261968c5e
maazghani <maazghani+github@gmail.com>
2015-06-12 11:59:52 -0700

    don't know why I did 2 bash scripts to begin

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 if [ "$BRANCH" == 'develop' ]; then
     GIT_COMMIT=`echo $COMMIT | head -c 5` 
     scp -r $GOROOT/foundation ottemo@$REMOTE_HOST:~/deploy/foundation-$GIT_COMMIT
-    scp $HOME/src/foundation-updater.sh ottemo@REMOTE_HOST:~/deploy/foundation-updater.sh
     ssh ottemo@$REMOTE_HOST "cd /home/ottemo/deploy/ && ln -sf foundation-$GIT_COMMIT foundation-latest" 
+    ssh ottemo@$REMOTE_HOST "sudo /etc/init.d/ottemo stop && cp ~/foundation/foundation ~/foundation/backup/foundation-$(date +%Y%m%d)"
+    ssh ottemo@REMOTE_HOST "cp ~/deploy/foundation-latest ~/foundation/foundation && sudo /etc/init.d/ottemo start"
 fi
