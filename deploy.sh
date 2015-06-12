71ec0ab1da5b1659a46b72d892fb6d9431e9b108
maazghani <maazghani+github@gmail.com>
2015-06-12 12:24:53 -0700

    syntax issue

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ if [ "$BRANCH" == 'feature/maaz_deploy_to_host' ]; then
     scp -r $GOROOT/bin/foundation ottemo@$REMOTE_HOST:~/deploy/foundation-$GIT_COMMIT
     ssh ottemo@$REMOTE_HOST "cd /home/ottemo/deploy/ && ln -sf foundation-$GIT_COMMIT foundation-latest" 
     ssh ottemo@$REMOTE_HOST "sudo /etc/init.d/ottemo stop && cp ~/foundation/foundation ~/foundation/backup/foundation-$(date +%Y%m%d)"
-    ssh ottemo@REMOTE_HOST "cp ~/deploy/foundation-latest ~/foundation/foundation && sudo /etc/init.d/ottemo start"
+    ssh ottemo@$REMOTE_HOST "cp ~/deploy/foundation-latest ~/foundation/foundation && sudo /etc/init.d/ottemo start"
 fi
