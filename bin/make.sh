01399dc6a1be651bd1c7ae0dc570e1aa4dafd504
Viktor Nazarov <vnazarov@speroteck.com>
2015-09-29 17:34:24 -0700

    [#102692186] makescript bugfixing

--- bin/make.sh
+++ bin/make.sh
@@ -4,12 +4,13 @@ WORKDIR=`pwd`
 OTTEMODIR="$(cd "$(dirname "$0")" && pwd)"
 OTTEMOPKG="github.com/ottemo/foundation"
 
-BRANCH=`git -C $OTTEMODIR rev-parse --abbrev-ref HEAD`
-BUILD=`git -C $OTTEMODIR rev-list origin/develop --count`
-DATE=`date --rfc-3339=second`
-DATE="${DATE/ /T}"
-HASH=`git -C $OTTEMODIR rev-parse --short --verify HEAD`
+DATE=`awk 'BEGIN{ print gensub(/(..)$/, ":\\\1", "g", strftime("%Y-%m-%dT%H:%M:%S%z")); exit }'`
 TAGS=""
+BUILD=`git -C $OTTEMODIR rev-list origin/develop --count`
+BRANCH=`git -C $OTTEMODIR rev-parse --abbrev-ref HEAD`
+HASH=`git -C $OTTEMODIR rev-parse --short --verify HEAD`
+
+GOVERSION=`go version | awk '{print gensub(/.*go([0-9]+[.][0-9]+).[0-9]+.*/, "\\\1", "1")}'`
 
 while test $# -gt 0; do
         case "$1" in
@@ -37,13 +38,17 @@ done
 
 cd $WORKDIR
 
-LDFLAGS="-ldflags \""
-LDFLAGS+="-X github.com/ottemo/foundation/app.buildDate=$DATE "
-LDFLAGS+="-X github.com/ottemo/foundation/app.buildTags=$TAGS "
-LDFLAGS+="-X github.com/ottemo/foundation/app.buildNumber=$BUILD "
-LDFLAGS+="-X github.com/ottemo/foundation/app.buildBranch=$BRANCH "
-LDFLAGS+="-X github.com/ottemo/foundation/app.buildHash=$HASH"
-LDFLAGS+="\""
+LDFLAGS="-ldflags '"
+LDFLAGS+="-X \"github.com/ottemo/foundation/app.buildDate=$DATE\" "
+LDFLAGS+="-X \"github.com/ottemo/foundation/app.buildTags=$TAGS\" "
+LDFLAGS+="-X \"github.com/ottemo/foundation/app.buildNumber=$BUILD\" "
+LDFLAGS+="-X \"github.com/ottemo/foundation/app.buildBranch=$BRANCH\" "
+LDFLAGS+="-X \"github.com/ottemo/foundation/app.buildHash=$HASH\" "
+LDFLAGS+="'"
+
+if [ "`awk "BEGIN{ if ($GOVERSION < 1.5) print 1 }"`" == "1" ]; then
+  LDFLAGS=${LDFLAGS//=/\" \"}
+fi
 
 if [ -z "$GOPATH" ]; then 
 REPLACE="/src/$OTTEMOPKG"
