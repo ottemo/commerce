e317eaf65912cb3e7fd8fec4e7e9c54ea4942eb6
James Vastbinder <james.vastbinder@gmail.com>
2016-01-07 15:34:35 -0800

    version bump for release of v1.1.02

--- app/decl.go
+++ app/decl.go
@@ -9,8 +9,8 @@ import (
 // Package global constants
 const (
 	ConstVersionMajor = 1
-	ConstVersionMinor = 0
-	ConstSprintNumber = 47
+	ConstVersionMinor = 1
+	ConstSprintNumber = 02
 
 	ConstConfigPathGroup      = "general"
 	ConstConfigPathAppGroup   = "general.app"
