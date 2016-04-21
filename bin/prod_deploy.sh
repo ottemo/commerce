8e4cb8628427e691b59f48d76c5935fc49ab5a68
James Vastbinder <james.vastbinder@gmail.com>
2016-04-21 23:36:12 +0000

    script for production deploys

---
+++ bin/prod_deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# vars for script
+PWD=$PWD
+SRCDIR=/home/ottemo/code/go/src/github.com/ottemo/foundation
+
+cd $SRCDIR
+# Build Foundation
+./bin/make.sh -tags mongo
+
+# stop the foundation service
+sudo service ottemo stop
+
+# Backup binaries and restart foundation
+cp ~/foundation/foundation ~/foundation/foundation.bak
+cp foundation ~/foundation/
+sudo service ottemo start
+
+# restore PWD
+cd $PWD
