19966940480fed1d755f06d4d89f9841ab8419e8
James Vastbinder <james.vastbinder@gmail.com>
2016-04-22 23:33:31 +0000

    version bump for hotfix release v1.1.161

--- app/decl.go
+++ app/decl.go
@@ -10,7 +10,7 @@ import (
 const (
 	ConstVersionMajor = 1
 	ConstVersionMinor = 1
-	ConstSprintNumber = 16
+	ConstSprintNumber = 161
 
 	ConstConfigPathGroup      = "general"
 	ConstConfigPathAppGroup   = "general.app"
