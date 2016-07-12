d6e8eeac101415e1ad8e9e64de2f0f2ebb060a2a
James Vastbinder <james.vastbinder@gmail.com>
2016-07-12 00:34:13 -0700

    add backup to deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,11 +4,12 @@
 SRCDIR=/home/ottemo/code/go/src/github.com/ottemo/foundation
 
 if [ "$BRANCH" == 'develop' ]; then
-    GIT_COMMIT=`echo $COMMIT | head -c 5`
+    #GIT_COMMIT=$( echo "$COMMIT" | head -c 5 )
     # grab the latest code
     ssh ottemo@$REMOTE_HOST "cd $SRCDIR && git stash && git checkout develop && git fetch --prune && git pull"
     # build locally after successful merge to develop
     ssh ottemo@$REMOTE_HOST "cd $SRCDIR && go get -t ./... && bash bin/make.sh -tags mongo"
     # backup the current binary and put the newly built binary into service
-    ssh ottemo@$REMOTE_HOST "sudo /etc/init.d/ottemo stop && cp $SRCDIR/foundation ~/foundation/foundation && sudo /etc/init.d/ottemo start"
+    ssh ottemo@$REMOTE_HOST "sudo service ottemo stop && mv $HOME/foundation/foundation $HOME/foundation/foundation.bak"
+    ssh ottemo@$REMOTE_HOST "cp $SRCDIR/foundation ~/foundation/foundation && sudo service ottemo start"
 fi
