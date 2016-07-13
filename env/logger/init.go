e3cf5a954bde450d44f49d8d64081757be05eb41
James Vastbinder <james@ottemo.io>
2016-07-13 16:21:33 -0700

    update logrus to use formatter in new repo (#374)

--- env/logger/init.go
+++ env/logger/init.go
@@ -4,14 +4,13 @@ import (
 	"os"
 
 	log "github.com/Sirupsen/logrus"
-	"github.com/Sirupsen/logrus/formatters/logstash"
+	"github.com/bshuster-repo/logrus-logstash-hook"
 	"github.com/ottemo/foundation/env"
 )
 
 // init makes package self-initialization routine
 func init() {
-	log.SetFormatter(&logstash.LogstashFormatter{Type: "ottemo_api"})
-
+	log.SetFormatter(&logrus_logstash.LogstashFormatter{Type: "ottemo_api"})
 	instance := new(DefaultLogger)
 	var _ env.InterfaceLogger = instance
 
