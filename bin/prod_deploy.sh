8757b4e73a32a627ba0c006ffe7e1e8e85f0bb88
James Vastbinder <james.vastbinder@gmail.com>
2016-08-30 15:17:58 -0700

    update production deploy script for foundation

--- bin/prod_deploy.sh
+++ bin/prod_deploy.sh
@@ -10,8 +10,13 @@ echo "BUILDING FOUNDATION"
 ./bin/make.sh -tags mongo
 
 # stop the foundation service
-sudo service ottemo stop
+echo "STOP FOUNDATION SERVICE"
+while sudo service ottemo stop >/dev/null 2>&1; do
+    echo "warning: foundation is still running"
+done
+echo "info: foundation has terminated"
 
+echo "DEPLOYING FOUNDATION"
 # Backup binaries and restart foundation
 cp ~/foundation/foundation ~/foundation/foundation.bak
 cp foundation ~/foundation/
