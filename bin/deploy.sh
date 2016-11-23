c550e950a284839c739e8633a743091df242133d
bikrus <bikrus@gmail.com>
2016-11-23 04:49:23 -0600

    *deploy.sh: only develop branch (#413)
    * *deploy.sh: only develop branch
    
    * fix ncd host name for shippable

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,10 +7,22 @@ SRCDIR=/home/ottemo/code/go/src/github.com/ottemo/foundation
 if [ "$BRANCH" == 'develop' ]; then
     #GIT_COMMIT=$( echo "$COMMIT" | head -c 5 )
     # grab the latest code
-    ssh ottemo@$REMOTE_HOST "cd $SRCDIR && git stash && git checkout develop && git fetch --prune && git pull"
-    # build locally after successful merge to develop
+
+    currentBranch=`ssh ottemo@$REMOTE_HOST "cd $SRCDIR && git symbolic-ref --quiet --short HEAD 2> /dev/null || git rev-parse --short HEAD 2> /dev/null || echo '(unknown)'"`
+    echo ""
+    echo "FOUNDATION BRANCH IS ${currentBranch}"
+
+    if [ "$currentBranch" == 'develop' ]; then
+        echo "GRAB THE LATEST CODE"
+        ssh ottemo@$REMOTE_HOST "cd $SRCDIR && git stash && git checkout develop && git fetch --prune && git pull"
+    fi
+
+    echo ""
+    echo "BUILD LOCALLY"
     ssh ottemo@$REMOTE_HOST "cd $SRCDIR && go get -t ./... && bash bin/make.sh -tags mongo"
-    # backup the current binary and put the newly built binary into service
+
+    echo ""
+    echo "BACKUP THE CURRENT BINARY AND PUT THE NEWLY BUILT BINARY INTO SERVICE"
     ssh ottemo@$REMOTE_HOST "sudo service ottemo stop && mv $HOME/foundation/foundation $HOME/foundation/foundation.bak"
     ssh ottemo@$REMOTE_HOST "cp $SRCDIR/foundation ~/foundation/foundation && sudo service ottemo start"
 fi
