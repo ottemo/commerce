5b6ced26e954f8176d1e72b85b7f21a77c74ef60
James Vastbinder <james.vastbinder@gmail.com>
2016-12-15 18:22:49 +0000

    release version 1.1.48
    Notes
    * add item index to shipstation exports

--- app/decl.go
+++ app/decl.go
@@ -10,7 +10,7 @@ import (
 const (
 	ConstVersionMajor = 1
 	ConstVersionMinor = 1
-	ConstSprintNumber = 47
+	ConstSprintNumber = 48
 
 	ConstConfigPathGroup      = "general"
 	ConstConfigPathAppGroup   = "general.app"
