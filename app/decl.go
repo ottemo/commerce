9285785b7b92d3369a3d5d38543a9170f9ee6dbe
James Vastbinder <james.vastbinder@gmail.com>
2016-12-20 05:03:02 +0000

    version bump to release emma integration

--- app/decl.go
+++ app/decl.go
@@ -10,7 +10,7 @@ import (
 const (
 	ConstVersionMajor = 1
 	ConstVersionMinor = 1
-	ConstSprintNumber = 48
+	ConstSprintNumber = 49
 
 	ConstConfigPathGroup      = "general"
 	ConstConfigPathAppGroup   = "general.app"
