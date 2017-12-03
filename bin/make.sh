059193ef95a2c6cc12dd13e6a63f4819dc69c049
James Vastbinder <james@ottemo.io>
2017-12-03 10:48:37 -0800

    add comments and only install glide once

--- bin/make.sh
+++ bin/make.sh
@@ -1,15 +1,20 @@
-#!/bin/bash
+#!/bin/env bash
 
 WORKDIR=`pwd`
 OTTEMODIR="$(cd "$(dirname "$0")" && pwd)"
 OTTEMOPKG="github.com/ottemo/foundation"
 
+# select the right version of awk
+#    on MacOS and Alpine you may need to install gawk
+#
 if [[ "$OSTYPE" == "darwin"*  ]] || [ -f /etc/alpine-release ] || [ -f /.dockerenv ] ; then
     AWK=gawk
 else
     AWK=awk
 fi
 
+# set up env vars for build status
+#
 DATE=`${AWK} 'BEGIN{ print gensub(/(..)$/, ":\\\1", "g", strftime("%Y-%m-%dT%H:%M:%S%z")); exit }'`
 TAGS=""
 BUILD=`git -C $OTTEMODIR rev-list origin/develop --count`
@@ -18,27 +23,29 @@ HASH=`git -C $OTTEMODIR rev-parse --short --verify HEAD`
 
 GOVERSION=`go version | ${AWK} '{print $3}' | awk '{sub(/go/,""); print}'`
 
+# handle flags
+#
 while test $# -gt 0; do
         case "$1" in
-                -tags)
-			shift
-                        TAGS+=$1
-			shift
-                        ;;
-                -wd|--work-dir)
-			shift
-			WORKDIR=$1
-                        shift
-                        ;;
-		-od|--ottemo-dir)
-			shift
-                        WORKDIR=$1
-			shift
-                        ;;
-		*)
-			echo $1
-			shift
-			;;
+            -tags)
+                shift
+                TAGS+=$1
+                shift
+                ;;
+            -wd|--work-dir)
+                shift
+                WORKDIR=$1
+                shift
+                ;;
+            -od|--ottemo-dir)
+                shift
+                WORKDIR=$1
+                shift
+                ;;
+            *)
+                echo $1
+                shift
+                ;;
         esac
 done
 
@@ -53,11 +60,12 @@ LDFLAGS+="-X \"github.com/ottemo/foundation/app.buildHash=$HASH\" "
 LDFLAGS+="'"
 
 # need to convert GOVERSION string to number
+#
 if [ "`${AWK} "BEGIN{ if (($GOVERSION +0) < 1.5) print 1 }"`" == "1" ]; then
   LDFLAGS=${LDFLAGS//=/\" \"}
 fi
 
-if [ -z "$GOPATH" ]; then 
+if [ -z "$GOPATH" ]; then
 REPLACE="/src/$OTTEMOPKG"
 export GOPATH="${OTTEMODIR/$REPLACE/}"
 fi
@@ -68,9 +76,17 @@ TAGS=$(echo $TAGS| sed 's/,/ /g')
 TAGS="-tags '$TAGS'"
 fi
 
-# install glide to $GOPATH and project dependencies
-go get github.com/Masterminds/glide
+# install glide to $GOPATH
+#     if it does not exist yet
+#
+if [ ! -f "$GOPATH/bin/glide" ]; then
+    echo "Glide not found, installing it.\n"
+    go get github.com/Masterminds/glide
+fi
+
+# install project dependencies
 $GOPATH/bin/glide install
+
 
 CMD="go build -a $TAGS $LDFLAGS $OTTEMOPKG"
 eval CGO_ENABLED=0 GOOS=linux $CMD
